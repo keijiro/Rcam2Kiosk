b78a7b657db6da1f03cce7d8e7a849e3603753e0
Keijiro Takahashi <keijiro@gmail.com>
2023-12-14 17:05:07 +0900

    Changes: Camera switching (3rd person view) improvements

--- RcamVisualizer/Assets/Scripts/System/SceneCameraController.cs
+++ RcamVisualizer/Assets/Scripts/System/SceneCameraController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.VFX;
+using Klak.Math;
 
 namespace Rcam2 {
 
@@ -12,51 +13,101 @@ public sealed class SceneCameraController : MonoBehaviour
 
     [field:Space]
     [field:SerializeField] public Transform Target { get; set; }
+    [field:SerializeField] public Transform Follower { get; set; }
     [field:SerializeField] public Transform LockPoint { get; set; }
-    [field:SerializeField] public Transform Floater { get; set; }
 
     [field:Space]
     [field:SerializeField] public GameObject Background { get; set; }
     [field:SerializeField] public GameObject Recolor { get; set; }
-    [field:SerializeField] public GameObject PointCloud { get; set; }
+
+    [field:Space]
+    [field:SerializeField] public VisualEffect PointCloud { get; set; }
+    [field:SerializeField] public VisualEffect ProjecterVfx { get; set; }
+    [field:SerializeField] public Color LineColor { get; set; } = Color.white;
 
     #endregion
 
     #region Private objects
 
-    bool _floating;
+    bool _locked;
+
+    #endregion
+
+    #region Predefined values
+
+    static class PropertyID
+    {
+        public static int LineColor = Shader.PropertyToID("Line Color");
+    }
+
+    #endregion
+
+    #region Camera switching (async operations)
+
+    async void LockCamera()
+    {
+        // Hierarchy change (parented to the lock point)
+        Target.parent = LockPoint;
+
+        // Tween to the zero point
+        for (var t = 0.0f; t < 0.25f; t += Time.deltaTime)
+        {
+            Target.localPosition = ExpTween.Step(Target.localPosition, Vector3.zero, 12);
+            Target.localRotation = ExpTween.Step(Target.localRotation, Quaternion.identity, 12);
+            await Awaitable.NextFrameAsync();
+        }
+
+        // Force reset
+        Target.localPosition = Vector3.zero;
+        Target.localRotation = Quaternion.identity;
+
+        // Effect visibilities
+        Background.SetActive(true);
+        Recolor.SetActive(true);
+        PointCloud.enabled = false;
+        ProjecterVfx.SetVector4(PropertyID.LineColor, Color.clear);
+    }
+
+    void UnlockCamera()
+    {
+        // Smooth follower reset to the current camera position
+        Follower.position = Target.position;
+        Follower.rotation = Target.rotation;
+
+        // Hierarhy change (parented to the smooth follower)
+        Target.parent = Follower;
+
+        // Effect visibilities
+        PointCloud.enabled = true;
+        Background.SetActive(false);
+        Recolor.SetActive(false);
+        ProjecterVfx.SetVector4(PropertyID.LineColor, LineColor);
+    }
 
     #endregion
 
     #region MonoBehaviour implementation
 
+    void Start()
+      => _locked = true;
+
     void Update()
     {
-        var prevFloating = _floating;
-
         // Button input
-        var locked = Input.GetButton(8);
-        var unlocked = Input.GetButton(9);
-        _floating = locked ? false : (unlocked ? true : prevFloating);
+        var lockButtonPressed = Input.GetButton(8);
+        var unlockButtonPressed = Input.GetButton(9);
 
-        if (_floating != prevFloating)
+        if (!_locked && lockButtonPressed)
         {
-            if (_floating)
-            {
-                Target.parent = Floater;
-                Target.localPosition = Vector3.zero;
-                Target.localRotation = Quaternion.identity;
-            }
-            else
-            {
-                Target.parent = LockPoint;
-                Target.localPosition = Vector3.zero;
-                Target.localRotation = Quaternion.identity;
-            }
-
-            Background.SetActive(!_floating);
-            Recolor.SetActive(!_floating);
-            PointCloud.SetActive(_floating);
+            // Lock action invoked
+            _locked = true;
+            LockCamera();
+        }
+        else if (_locked && unlockButtonPressed)
+        {
+            // Unlock action invoked
+            _locked = false;
+            UnlockCamera();
         }
     }
 
